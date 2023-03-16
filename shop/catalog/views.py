23d5da5b928d370a9af9dcd84899d1d3cdf2f515
MineevHome <i@mineevburyat.ru>
2023-03-16 21:31:24 +0800

    добавил файл index

--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello django")
+    return render(request, 'catalog/index.html')
