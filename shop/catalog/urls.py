85148c05abd983815a23b52a25f4fe3c4d9ab040
MineevHome <i@mineevburyat.ru>
2023-03-16 21:51:45 +0800

    статичные файлы на странице продуктов

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, products
 
 urlpatterns = [
     path('', index),
+    path('products', products)
 ]
--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'catalog/index.html')
+
+def products(request):
+    return render(request, 'catalog/products.html')
