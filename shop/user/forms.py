2c81ef7d9acd18058439eb78be5d6a80fb8ce2fe
MineevHome <i@mineevburyat.ru>
2023-03-18 02:30:18 +0800

    форма с кастомизированными полями и дизайном

--- shop/user/forms.py
+++ shop/user/forms.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.forms import AuthenticationForm
 from .models import User
+from django import forms
 
 class FormAuth(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control py-4',
+            'plaseholder': 'Введите имя пользователя'
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+        'class': 'form-control py-4',
+        'plaseholders': 'Введите пароль'
+        }
+    ))
     class Meta:
         model = User
         fields = ('username', 'password')
