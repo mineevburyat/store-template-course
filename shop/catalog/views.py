bb93b6918f1758e00f764bb20d0231b9be503c0d
Mineev <mineev.buryat@gmail.com>
2023-03-20 13:03:22 +0800

    пагинация

--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from .models import Product, ProductCategory, Basket
 from user.models import User
+from django.core.paginator import Paginator
 
 # Create your views here.
 # def index(request):
@@ -9,14 +10,23 @@ from user.models import User
 #     })
 
 def products(request, category_id=None):
+    PER_PAGE = 6
     if category_id:
         category = ProductCategory.objects.get(id=category_id)
         products = Product.objects.filter(category=category)
     else:
         products = Product.objects.all()
+    page = request.GET.get('page', 1)
+    if not isinstance(page, int):
+        if page.isdigit():
+            page = int(page)
+        else:
+            page = 1
+    paginator = Paginator(products, per_page=PER_PAGE)
+    page_products = paginator.page(page)
     return render(request, 'catalog/products.html', context={
         'title': 'Store - продукты',
-        'products': products,
+        'products': page_products,
         'category': ProductCategory.objects.all(),
         'category_id': category_id
     })
