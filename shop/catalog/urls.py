b71b20301049b82ad0140f1361ec09f97ee03129
Mineev <mineev.buryat@gmail.com>
2023-03-22 21:12:23 +0800

    CBV products ListView - пагинация, контекст

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import products, add_to_basket, remove_from_basket
+from .views import add_to_basket, remove_from_basket, ProductsListView
 
 app_name='product'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('category/<int:category_id>', products, name='category'),
+    path('', ProductsListView.as_view(), name='index'),
+    path('category/<int:category_id>', ProductsListView.as_view(), name='category'),
     path('basket/add/<int:product_id>', add_to_basket, name='add_product'),
     path('basket/del/<int:product_id>', remove_from_basket, name='remove_product_from_basket')
 ]
--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -3,6 +3,8 @@ from .models import Product, ProductCategory, Basket
 from user.models import User
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 
 # Create your views here.
 # def index(request):
@@ -10,27 +12,49 @@ from django.core.paginator import Paginator
 #         'title': 'Store - магазин',
 #     })
 
-def products(request, category_id=None):
-    PER_PAGE = 3
-    if category_id:
-        category = ProductCategory.objects.get(id=category_id)
-        products = Product.objects.filter(category=category)
-    else:
-        products = Product.objects.all()
-    page = request.GET.get('page', 1)
-    if not isinstance(page, int):
-        if page.isdigit():
-            page = int(page)
+class ProductsListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'catalog/products.html'
+    paginate_by = 3
+    
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            category = ProductCategory.objects.get(id=category_id)
+            products = Product.objects.filter(category=category)
         else:
-            page = 1
-    paginator = Paginator(products, per_page=PER_PAGE)
-    page_products = paginator.page(page)
-    return render(request, 'catalog/products.html', context={
-        'title': 'Store - продукты',
-        'products': page_products,
-        'category': ProductCategory.objects.all(),
-        'category_id': category_id
-    })
+            products = Product.objects.all()
+        return products
+    
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['title'] = 'Mагазин Store - товары'
+        context['category'] = ProductCategory.objects.all()
+        context['category_id'] = self.kwargs.get('category_id')
+        return context
+
+# def products(request, category_id=None):
+#     PER_PAGE = 3
+#     if category_id:
+#         category = ProductCategory.objects.get(id=category_id)
+#         products = Product.objects.filter(category=category)
+#     else:
+#         products = Product.objects.all()
+#     page = request.GET.get('page', 1)
+#     if not isinstance(page, int):
+#         if page.isdigit():
+#             page = int(page)
+#         else:
+#             page = 1
+#     paginator = Paginator(products, per_page=PER_PAGE)
+#     page_products = paginator.page(page)
+#     return render(request, 'catalog/products.html', context={
+#         'title': 'Store - продукты',
+#         'products': page_products,
+#         'category': ProductCategory.objects.all(),
+#         'category_id': category_id
+#     })
 
 @login_required
 def add_to_basket(request, product_id):
