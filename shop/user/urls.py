c11964bfdb0f2ae08284aa66f261e8d066d8e42f
Mineev <mineev.buryat@gmail.com>
2023-03-22 23:23:31 +0800

    CBV register user with mixin messages success

--- shop/user/urls.py
+++ shop/user/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import register_user, profile, AuthView, MyLogoutView
+from .views import profile, AuthView, MyLogoutView, RegisterUser
 
 
 app_name='user'
@@ -8,7 +8,7 @@ app_name='user'
 urlpatterns = [
     # path('logout', products, name='index'),
     path('login', AuthView.as_view(), name='login'),
-    path('register', register_user, name='register'),
+    path('register', RegisterUser.as_view(), name='register'),
     path('profile', profile, name='profile'),
     path('logout', MyLogoutView.as_view(), name='logout'),
 ]
