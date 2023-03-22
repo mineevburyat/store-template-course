7bb6dced266182ac88880aabe673992bc362630a
Mineev <mineev.buryat@gmail.com>
2023-03-22 23:22:59 +0800

    CBV Register user

--- shop/user/views.py
+++ shop/user/views.py
@@ -4,27 +4,23 @@ from django.contrib import auth, messages
 from django.urls import reverse
 from catalog.models import Basket
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from .models import User
+from django.contrib.messages.views import SuccessMessageMixin
 
 class AuthView(LoginView):
     template_name = 'user/login.html'
     authentication_form = FormAuth
     next_page = '/'
 
+class RegisterUser(SuccessMessageMixin, CreateView):
+    model = User
+    template_name = 'user/register.html'
+    form_class = FormRegister
+    success_url = '/user/login'
+    success_message = 'успешно зарегестрирован'
 
-def register_user(request):
-    if request.method == 'POST':
-        form = FormRegister(data=request.POST)
-        if form.is_valid():
-            messages.success(request, 'успешно зарегестрировались!')
-            form.save()
-            HttpResponseRedirect(reverse('user:login'))
-    else:
-        form = FormRegister()
-    return render(request, 
-                  'user/register.html',
-                  context = {
-                      'form': form,
-                  })
+
 
 def profile(request):
     if request.method == 'POST' and request.user.is_authenticated:
