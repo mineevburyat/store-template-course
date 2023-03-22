a7bbb2879c103acb6ab99f6c20536a0c39929da8
Mineev <mineev.buryat@gmail.com>
2023-03-22 22:57:52 +0800

    Logout

--- shop/user/urls.py
+++ shop/user/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import register_user, profile, logout, AuthView
+from .views import register_user, profile, AuthView, MyLogoutView
 
 
 app_name='user'
@@ -10,5 +10,5 @@ urlpatterns = [
     path('login', AuthView.as_view(), name='login'),
     path('register', register_user, name='register'),
     path('profile', profile, name='profile'),
-    path('logout', logout, name='logout'),
+    path('logout', MyLogoutView.as_view(), name='logout'),
 ]
