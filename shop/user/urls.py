a75e1c99c0157ab89e6f99153fc2109bba7db29f
Mineev <mineev.buryat@gmail.com>
2023-03-23 10:24:58 +0800

    CBV profile change

--- shop/user/urls.py
+++ shop/user/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import profile, AuthView, MyLogoutView, RegisterUser
+from .views import profile, AuthView, MyLogoutView, RegisterUserView, ProfileEditView
 
 
 app_name='user'
@@ -8,7 +8,7 @@ app_name='user'
 urlpatterns = [
     # path('logout', products, name='index'),
     path('login', AuthView.as_view(), name='login'),
-    path('register', RegisterUser.as_view(), name='register'),
-    path('profile', profile, name='profile'),
+    path('register', RegisterUserView.as_view(), name='register'),
+    path('profile/<int:pk>', ProfileEditView.as_view(), name='profile'),
     path('logout', MyLogoutView.as_view(), name='logout'),
 ]
--- shop/user/views.py
+++ shop/user/views.py
@@ -4,7 +4,7 @@ from django.contrib import auth, messages
 from django.urls import reverse
 from catalog.models import Basket
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import User
 from django.contrib.messages.views import SuccessMessageMixin
 
@@ -13,14 +13,23 @@ class AuthView(LoginView):
     authentication_form = FormAuth
     next_page = '/'
 
-class RegisterUser(SuccessMessageMixin, CreateView):
+class RegisterUserView(SuccessMessageMixin, CreateView):
     model = User
     template_name = 'user/register.html'
     form_class = FormRegister
     success_url = '/user/login'
     success_message = 'успешно зарегестрирован'
 
-
+class ProfileEditView(UpdateView):
+    model = User
+    form_class = FormChangeProfile
+    template_name = 'user/profile.html'
+    success_url = '/user/profile'
+    # success_message = 'профиль изменен'
+    
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['basket'] = Basket.objects.filter(user=self.request.user)
 
 def profile(request):
     if request.method == 'POST' and request.user.is_authenticated:
