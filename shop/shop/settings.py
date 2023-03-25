9b23476983e2b6eca90b96f0ab58a2d2f8983ed8
Mineev <mineev.buryat@gmail.com>
2023-03-25 13:35:03 +0800

    fix settings

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -128,11 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / 'static'
+else:
+    STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
