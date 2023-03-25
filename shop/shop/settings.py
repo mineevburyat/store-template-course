ae848e6192b92954511acff42a8741b691d3e9f3
Mineev <mineev.buryat@gmail.com>
2023-03-25 13:31:45 +0800

    static root

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
