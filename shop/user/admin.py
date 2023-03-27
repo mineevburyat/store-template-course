79be764456b03ed429bcf26e91eef26c56040b29
MineevHome <i@mineevburyat.ru>
2023-03-28 00:23:25 +0800

    plesure admin users

--- shop/user/admin.py
+++ shop/user/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from .models import User, VerifideUser
 
-# Register your models here.
-admin.site.register(User)
-# admin.site.register(VerifideUser)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_verified_email')
+    fields = (('first_name', 'last_name', 'username', 'email'), 
+              ('is_superuser', 'is_staff', 'is_verified_email', 'is_active'), 
+              ('date_joined', 'last_login'), 'image',
+              ('groups', 'user_permissions'), 'password')
+    readonly_fields = ('password', 'email', 'last_login', 'date_joined')
 
 
 @admin.register(VerifideUser)
-class VerifiedUser(admin.ModelAdmin):
+class VerifiedUserAdmin(admin.ModelAdmin):
     list_display = ('code', 'user', 'created', 'expiration')
     fields = (('code', 'user'), ('created', 'expiration'))
     readonly_fields = ('created', 'user', 'code')
